0774cec8157e55e7fbcde1a2b21a7c72eccd86ac
DylanWard14 <dylan.ward@iinet.net.au>
2019-03-14 21:11:31 +1000

    Imports yargs and setup basic console commands

--- app.js
+++ app.js
@@ -1,6 +1,60 @@
 const validator = require('validator');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-console.log(getNotes());
+// Customize yargs version
+yargs.version('1.1.0');
 
-console.log(validator.isEmail("dylan.wardiinet.net.au"));
+// Create add command
+yargs.command({
+    command:'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'The body of the note',
+            demand: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv)
+    {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
+    }
+});
+
+yargs.command({
+    command:'remove',
+    describe: 'Remove a note',
+    handler: function ()
+    {
+        console.log('Removing the note');
+    }
+});
+
+yargs.command({
+    command:'list',
+    describe: 'list all the notes',
+    handler: function ()
+    {
+        console.log('Listing out all notes');
+    }
+});
+
+yargs.command({
+    command:'read',
+    describe: 'read a note',
+    handler: function ()
+    {
+        console.log('Reading the note');
+    }
+});
+
+// add, remove, read, list
+
+yargs.parse();
